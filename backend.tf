f59a50635bbb5b952b6dfdc55b27f67c3d69a498
MuhammadOwais28 <owaisamin862@gmail.com>
2025-01-30 12:51:44 +0500

    error resolved

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mytf-statefile-s3-293893048194"
-    key    = "state"
-    region = "us-east-1"
+    bucket         = "mytf-statefile-s3-29389304819452"
+    key            = "state"
+    region         = "us-east-1"
     dynamodb_table = "terraform-stlock"
   }
 }
